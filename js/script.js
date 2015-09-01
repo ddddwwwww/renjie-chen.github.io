9fd7dddaebc553ac9a1c5396cc857b44024558c7
renjie-chen <1723588505@qq.com>
2015-09-01 11:29:40 +0800

    Site updated: 2015-09-01 11:29:40

--- js/script.js
+++ js/script.js
@@ -1,7 +1,60 @@
 (function($){
-  // Nav bar toggle
-  $('#main-nav-toggle').on('click', function(){
-    $('.nav-container-inner').slideToggle();
+  /*toTop start*/
+  // When to show the scroll link
+  // higher number = scroll link appears further down the page
+  var upperLimit = 1000;
+  // Our scroll link element
+  var scrollElem = $('#totop');
+  // Scroll to top speed
+  var scrollSpeed = 500;
+  // Show and hide the scroll to top link based on scroll position
+  $(window).scroll(function() {
+    var scrollTop = $(document).scrollTop();
+    if (scrollTop > upperLimit) {
+      $(scrollElem).stop().fadeTo(300, 1); // fade back in
+    } else {
+      $(scrollElem).stop().fadeTo(300, 0); // fade out
+    }
+  });
+
+  // Scroll to top animation on click
+  $(scrollElem).click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, scrollSpeed);
+    return false;
+  });
+  /*toTop end*/
+  // Search
+  var $searchWrap = $('#search-form-wrap'),
+    isSearchAnim = false,
+    searchAnimDuration = 200;
+
+  var startSearchAnim = function(){
+    isSearchAnim = true;
+  };
+
+  var stopSearchAnim = function(callback){
+    setTimeout(function(){
+      isSearchAnim = false;
+      callback && callback();
+    }, searchAnimDuration);
+  };
+
+  $('#nav-search-btn').on('click', function(){
+    if (isSearchAnim) return;
+
+    startSearchAnim();
+    $searchWrap.addClass('on');
+    stopSearchAnim(function(){
+      $('.search-form-input').focus();
+    });
+  });
+
+  $('.search-form-input').on('blur', function(){
+    startSearchAnim();
+    $searchWrap.removeClass('on');
+    stopSearchAnim();
   });
 
   // Share
@@ -11,42 +64,62 @@
     e.stopPropagation();
 
     var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
+      type = $this.attr('data-share'),
       offset = $this.offset();
 
-    if ($('#' + id).length){
-      var box = $('#' + id);
+    if (type == 'baidu') {
+      var box = $('#article-share-box');
+      shareDataUrl = $this.attr('data-url');
+      shareDataTitle = $this.attr('data-title');
 
       if (box.hasClass('on')){
         box.removeClass('on');
         return;
       }
-    } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
-          '</div>',
-        '</div>'
-      ].join('');
 
-      var box = $(html);
+      $('.article-share-box.on').hide();
 
-      $('body').append(box);
-    }
+      box.css({
+        top: offset.top + 25,
+        left: offset.left - 25
+      }).addClass('on');
+    } else{
+      var url = $this.attr('data-url'),
+      encodedUrl = encodeURIComponent(url),
+      id = 'article-share-box-' + $this.attr('data-id');
 
-    $('.article-share-box.on').hide();
+      if ($('#' + id).length){
+        var box = $('#' + id);
 
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
+        if (box.hasClass('on')){
+          box.removeClass('on');
+          return;
+        }
+      } else {
+        var html = [
+          '<div id="' + id + '" class="article-share-box">',
+            '<input class="article-share-input" value="' + url + '">',
+            '<div class="article-share-links">',
+              '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+              '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
+              '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
+              '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
+            '</div>',
+          '</div>'
+        ].join('');
+
+        var box = $(html);
+
+        $('body').append(box);
+      }
+
+      $('.article-share-box.on').hide();
+
+      box.css({
+        top: offset.top + 25,
+        left: offset.left
+      }).addClass('on');
+    };
   }).on('click', '.article-share-box', function(e){
     e.stopPropagation();
   }).on('click', '.article-share-box-input', function(){
@@ -79,8 +152,32 @@
     $('.fancybox').fancybox();
   }
 
-  //Back to top
-  $("#back-to-top").on('click', function(){  
-    $('body,html').animate({scrollTop:0}, 600);
-  }); 
+  // Mobile nav
+  var $container = $('#container'),
+    isMobileNavAnim = false,
+    mobileNavAnimDuration = 200;
+
+  var startMobileNavAnim = function(){
+    isMobileNavAnim = true;
+  };
+
+  var stopMobileNavAnim = function(){
+    setTimeout(function(){
+      isMobileNavAnim = false;
+    }, mobileNavAnimDuration);
+  }
+
+  $('#main-nav-toggle').on('click', function(){
+    if (isMobileNavAnim) return;
+
+    startMobileNavAnim();
+    $container.toggleClass('mobile-nav-on');
+    stopMobileNavAnim();
+  });
+
+  $('#wrap').on('click', function(){
+    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
+
+    $container.removeClass('mobile-nav-on');
+  });
 })(jQuery);
